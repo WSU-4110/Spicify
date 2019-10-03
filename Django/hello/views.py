446dec16de59e8e5b2e1638af4683c5e936b0e8e
Travis-Preston <55240319+Travis-Preston@users.noreply.github.com>
2019-10-03 09:20:43 -0400

    more changes

--- Django/hello/views.py
+++ Django/hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from hello import SpicifyLogin
 
 def home(request):
-    return HttpResponse("Your Name: " + SpicifyLogin.displayname + " --------------- Your Followers: " + str(SpicifyLogin.followers) + "YEEEEt")
+    return HttpResponse("Your Name: " + SpicifyLogin.displayname + " --------------- Your Followers: " + str(SpicifyLogin.followers))
