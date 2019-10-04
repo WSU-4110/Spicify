14101c201c9eb8fa67c4afc6d029b1e16e255aa9
Travis-Preston <55240319+Travis-Preston@users.noreply.github.com>
2019-10-04 12:54:44 -0400

    Deleting old stuff

--- Django/hello/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-from hello import SpicifyLogin
-
-def home(request):
-    return HttpResponse("Your Name: " + SpicifyLogin.displayname + " --------------- Your Followers: " + str(SpicifyLogin.followers))
