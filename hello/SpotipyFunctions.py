425ab4d0109a9539ed0e5b169efbd775b8dbc973
Travis-Preston <55240319+Travis-Preston@users.noreply.github.com>
2019-10-06 19:08:32 -0400

    Sprint 1 (Almost Done)
    The master branch was not working until this point. The master branch was deleted and redone with working files. All that is needed now is a webpage for the main page of the application with a button that plays the "Study" playlist.

---
+++ hello/SpotipyFunctions.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import json
+import spotipy
+import webbrowser
+import spotipy.util as util
+from json.decoder import JSONDecodeError
+
+
+username = "22kpgi2vtrlebcei6eu37db7y"
+scope = 'user-read-private user-read-playback-state user-modify-playback-state'
+
+#Easily Install the latest version of spotipy for playback functions:
+#pip install git+https://github.com/plamere/spotipy.git --upgrade 
+
+# 22kpgi2vtrlebcei6eu37db7y
+
+# file path: C:\Spicify\hello
+
+# python SpotipyFunctions.py
+
+try:
+
+    token = util.prompt_for_user_token(username,scope,client_id='8ff0ccc6f1fb460e8fabbe33e0e42112',client_secret='03d0e4de0957434abcc60660045d7cfc',redirect_uri='https://www.google.com/')
+
+except:
+
+    os.remove(f".cache-{username}")
+    token = util.prompt_for_user_token(username,scope,client_id='8ff0ccc6f1fb460e8fabbe33e0e42112',client_secret='03d0e4de0957434abcc60660045d7cfc',redirect_uri='https://www.google.com/')
+
+spotifyObject = spotipy.Spotify(auth=token)
+
+devices = spotifyObject.devices()
+deviceID = devices['devices'][0]['id']
+
+user = spotifyObject.current_user()
+displayname = user['display_name']
+followers = user['followers']['total']
+
+Playlists = spotifyObject.current_user_playlists()['items'][0]['uri']
+
+def beginPlaylist():
+    spotifyObject.start_playback(device_id=deviceID, context_uri=Playlists)
+
+beginPlaylist()
---
+++ hello/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+import SpotipyFunctions
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'hello/home.html')
+
+def hello_there(request, name):
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': SpotipyFunctions.displayname,
+            'followers': SpotipyFunctions.followers
+        }
+    )
+
+def category(request):
+    return render(request, 'hello/category.html')
+
+def layout(request):
+    return render(request, 'hello/layout.html')
