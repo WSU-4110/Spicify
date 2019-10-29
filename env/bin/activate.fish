1d7a14261aeeaa6ab389ccde9b0141fdcee599d2
Travis-Preston <55240319+Travis-Preston@users.noreply.github.com>
2019-10-29 10:45:24 -0400

    Putting Year Range functions in seperate file

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/travispreston/Desktop/DatabaseProject/Spicify/env"
+set -gx VIRTUAL_ENV "/Users/travispreston/Desktop/Spicify/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
