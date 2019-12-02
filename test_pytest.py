c83ebe63288741408d28de61f923baac0ab8005a
Travis-Preston <55240319+Travis-Preston@users.noreply.github.com>
2019-12-02 16:29:53 -0500

    Fixing

--- test_pytest.py
+++ test_pytest.py
@@ -1,4 +1,19 @@
 from hello import YearRangePlaylist
 
+def test_30sPlaylist():
+    assert YearRangePlaylist.yearRangePlaylistClass().thirtiesPlaylist() == 30
+
+def test_40sPlaylist():
+    assert YearRangePlaylist.yearRangePlaylistClass().fourtiesPlaylist() == 30
+
+def test_50sPlaylist():
+    assert YearRangePlaylist.yearRangePlaylistClass().fiftiesPlaylist() == 30
+
+def test_60sPlaylist():
+    assert YearRangePlaylist.yearRangePlaylistClass().sixtiesPlaylist() == 30
+
+def test_80sPlaylist():
+    assert YearRangePlaylist.yearRangePlaylistClass().eightiesPlaylist() == 30
+    
 def test_70sPlaylist():
     assert YearRangePlaylist.yearRangePlaylistClass().SeventiesPlaylist() == 30
