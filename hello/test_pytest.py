9afb511ee31466ddae9b891985c9513292d860a5
Gianna Marchetti <gianna.marchetti@wayne.edu>
2019-12-04 09:59:16 -0500

    travis

--- hello/test_pytest.py
+++ hello/test_pytest.py
@@ -1,10 +1,11 @@
 # from hello import YearRangePlaylist
+import spotipy
 import pytest
 songs = []
 
 username = '22kpgi2vtrlebcei6eu37db7y'
 scope = 'playlist-read-collaborative playlist-modify-private playlist-modify-public playlist-read-private user-modify-playback-state user-read-currently-playing user-read-playback-state user-read-private user-read-email user-library-modify user-library-read user-follow-modify user-follow-read user-read-recently-played user-top-read streaming app-remote-control'
-token = 'BQDZJLWEE4x2j7ud6pQ4LmCWNPsxWOY4c6H3K8z8uYZk9-Q1jOxnNS2CVmcwQkQt_CSu5l4P2-hRcmwKi8UKBgehbBipP3kVoKeqEGfUe5cFc1GjtjFe2onyZnRohjJ8Ym_mkLphi6z2hRshSU-0sh0Q-UKgOgJ01X9fzDDQKMdkm8_-Wp4KMfAP626vOF0WLtnVto7Xy1qGPVbYppiQWc6NgNgAJxArZwvTwsod_HxdESaDAom9n1pevbWa5eM2Tqe2H8fy1mcIkdk-o3Hq3A", "token_type": "Bearer", "expires_in": 3600, "scope": "app-remote-control playlist-modify-private playlist-modify-public playlist-read-collaborative playlist-read-private streaming user-follow-modify user-follow-read user-library-modify user-library-read user-modify-playback-state user-read-currently-playing user-read-email user-read-playback-state user-read-private user-read-recently-played user-top-read", "expires_at": 1575390495, "refresh_token": "AQDD9CbyEXyprfuE7DQB9lePa0dYTaUcihv5DDsH08Fn-UDdBa4YxoIHOKzDV3eyxcBr4W1RvvGxvULd3BF2teSSnfr3dHHfTu8_AjgWfaw_8VZsGewr4lkKq5vEZP-G_FA'
+token = 'BQBV0Kfdhy9C4lAHEIY40l6se4HdeUz9fUCpC1XGhw1T-3AeZXYO5uAUGgQwjcWT6RVW6Lvb3SUuQmPy9WU0AdPPVkamoXpyHf8bFBECGFTJotag-ZAbHM0tXfgau5Amz7XLpeHiqKXPR-2_UjSRy8_ND0KXGehWiOMLiMTZ-n8ZKH5sqmjscpQDLMfkWlqI0e6AU3UHrEWCzWpVCNAS0tUpsUN2hAxK0Eg6CoYffheJ2xX_fpUIPgYmZnU1f7hC03ZyYa6_J6UJArDIcHcGPw", "token_type": "Bearer", "expires_in": 3600, "refresh_token": "AQDFL3kgini8AUy8UDEtAtXzi82qB11ZhkJHICwjFKvfig9hBlHjcIF_idoxnEQX9JeqSrg2NUZ6weeVwKhXJpJMd_B9rINfM23zT-HHdj2ZZ_SYTvpM2pFKwQdtP8Uosxo'
 # token will expire must be renewed before test is run to refresh token uncomment line 2 of test_pytest.py
 
 def test_unclepostyPlaylist():
@@ -173,20 +174,3 @@ def test_timmcgrawPlaylist():
 
 
 
-def test_30sPlaylist():
-    assert YearRangePlaylist.yearRangePlaylistClass().thirtiesPlaylist() == 30
-
-def test_40sPlaylist():
-    assert YearRangePlaylist.yearRangePlaylistClass().fourtiesPlaylist() == 30
-
-def test_50sPlaylist():
-    assert YearRangePlaylist.yearRangePlaylistClass().fiftiesPlaylist() == 30
-
-def test_60sPlaylist():
-    assert YearRangePlaylist.yearRangePlaylistClass().sixtiesPlaylist() == 30
-
-def test_80sPlaylist():
-    assert YearRangePlaylist.yearRangePlaylistClass().eightiesPlaylist() == 30
-    
-def test_70sPlaylist():
-    assert YearRangePlaylist.yearRangePlaylistClass().SeventiesPlaylist() == 30
